c69321927e48acbcc7e7089d2722731cde6c295a
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 21:45:32 +0500

    Initial commit

---
+++ src/main/java/org/example/model/Person.java
@@ -0,0 +1,29 @@
+package org.example.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Person")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+}
