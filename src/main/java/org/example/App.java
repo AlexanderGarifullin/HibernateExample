adfd295aac81810f4ec4e22670798185436037dc
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-13 03:08:29 +0500

    feat: make cascading on Persist between Person and Item

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,10 +6,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+
 
 /**
  * Hello world!
@@ -28,59 +27,12 @@ public class App
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 4);
+            Person person = new Person("Test cascade", 30);
 
-            Item item = session.get(Item.class, 1);
+            Item item = new Item("Test casc item", person);
+            person.setItems(new ArrayList<>(Collections.singletonList(item)));
 
-            item.getOwner().getItems().remove(item);
-
-            item.setOwner(person);
-            person.getItems().add(item);
-
-//            Person person = session.get(Person.class, 2);
-//
-//            session.remove(person);
-//
-//            person.getItems().forEach(i->i.setOwner(null));
-
-//            Person person = session.get(Person.class, 3);
-//
-//            List<Item> items = person.getItems();
-//
-//            for (Item item : items) {
-//                session.remove(item);
-//            }
-//
-//            person.getItems().clear();
-
-//            Person person = new Person("Test p", 30);
-//
-//            Item newItem = new Item("Tem hib 2", person);
-//
-//            person.setItems(new ArrayList<>(Collections.singletonList(newItem)));
-//
-//            session.save(person);
-//            session.save(newItem);
-
-//            Person person = session.get(Person.class, 2);
-//
-//            Item newItem = new Item("Hibernate new item", person);
-//
-//            person.getItems().add(newItem);
-//
-//            session.save(newItem);
-
-//            Item item = session.get(Item.class, 5);
-//            System.out.println(item);
-//
-//            Person person = item.getOwner();
-//            System.out.println(person);
-
-//            Person person = session.get(Person.class, 3);
-//            System.out.println(person);
-//
-//            List<Item> items  = person.getItems();
-//            System.out.println(items);
+            session.persist(person);
 
             session.getTransaction().commit();
         } finally {
