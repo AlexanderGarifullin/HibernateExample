3319ff019915152d4e3ab407fd4445eb173f2bc2
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-12 22:18:00 +0500

    feat: make add request with hibernate

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -6,21 +6,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="Person")
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
@@ -28,4 +25,9 @@ public class Person {
 
     @Column(name = "age")
     private int age;
+
+    public Person(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
 }
